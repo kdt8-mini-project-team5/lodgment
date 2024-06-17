6e017493bb38a2d2cb5f7d11d957776aaae00477
Domae-back-end <ty_ty123@naver.com>
2024-06-17 21:07:33 +0900

    init

---
+++ src/main/java/com/lodgment/lodgment/LodgmentApplication.java
@@ -0,0 +1,13 @@
+package com.lodgment.lodgment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lodgment/lodgment/LodgmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodgment.lodgment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
