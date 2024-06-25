246319e39ea8829d0a3fc8ccbd0c76a269a0c476
Domae-back-end <ty_ty123@naver.com>
2024-06-25 18:01:35 +0900

    !HOTFIX : 비밀번호 검증 어노테이션 수정

--- src/main/java/com/accommodation/accommodation/global/validation/type/PasswordValidation.java
+++ src/main/java/com/accommodation/accommodation/global/validation/type/PasswordValidation.java
@@ -1,6 +1,7 @@
 package com.accommodation.accommodation.global.validation.type;
 
 import com.accommodation.accommodation.global.validation.EmailValidator;
+import com.accommodation.accommodation.global.validation.PasswordValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
@@ -11,7 +12,7 @@ import java.lang.annotation.Target;
 
 @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = EmailValidator.class)
+@Constraint(validatedBy = PasswordValidator.class)
 public @interface PasswordValidation {
 
     String message() default "Invalid password format";
