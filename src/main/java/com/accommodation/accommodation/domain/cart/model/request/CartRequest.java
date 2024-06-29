ad1815b56f1b7ddabb32faba3fb4ed99222955fd
sinsj5858 <goddnjswns58@gmail.com>
2024-06-29 17:23:29 +0900

    feat : 장바구니 추가 request의 CheckInCheckOutDateValidation 구현

--- src/main/java/com/accommodation/accommodation/domain/cart/model/request/CartRequest.java
+++ src/main/java/com/accommodation/accommodation/domain/cart/model/request/CartRequest.java
@@ -1,12 +1,14 @@
 package com.accommodation.accommodation.domain.cart.model.request;
 
+import com.accommodation.accommodation.global.validation.type.CheckInCheckOutDateValidation;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
+@CheckInCheckOutDateValidation(checkInDate = "checkInDate",checkOutDate = "checkOutDate")
 public record CartRequest(
     @NotNull Long roomId,
     @NotNull int people,
-    LocalDate checkInDate,
-    LocalDate checkOutDate
+    @NotNull LocalDate checkInDate,
+    @NotNull LocalDate checkOutDate
 ) {
 }
