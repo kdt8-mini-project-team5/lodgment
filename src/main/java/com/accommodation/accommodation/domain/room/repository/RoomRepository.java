94c31a94f670c786c80e9810643cf7ad32f30930
sinsj5858 <goddnjswns58@gmail.com>
2024-07-01 16:12:20 +0900

    fix : 충돌 해결

--- src/main/java/com/accommodation/accommodation/domain/room/repository/RoomRepository.java
+++ src/main/java/com/accommodation/accommodation/domain/room/repository/RoomRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> , RoomRepositoryCustom{
 
     @Query("SELECT r FROM Room r JOIN FETCH r.images where r.accommodation.id = :accommodationId")
     List<Room> findRoomDetailByAccommodationId(Long accommodationId);
