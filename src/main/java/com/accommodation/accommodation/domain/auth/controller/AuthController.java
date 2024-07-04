f6a578dfb8d94d2d64956ff00835f6ef4ac65804
Domae-back-end <ty_ty123@naver.com>
2024-07-04 15:53:16 +0900

    !HOTFIX : 배포 v3.1.1
    log 추가

--- src/main/java/com/accommodation/accommodation/domain/auth/controller/AuthController.java
+++ src/main/java/com/accommodation/accommodation/domain/auth/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.accommodation.accommodation.domain.auth.controller;
 
+import com.accommodation.accommodation.domain.auth.config.model.CustomUserDetails;
 import com.accommodation.accommodation.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api")
 public class AuthController {
 
     private final AuthService authService;
 
     @GetMapping("/check")
-    public ResponseEntity checkLoggedIn() {
+    public ResponseEntity checkLoggedIn(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        log.info(customUserDetails.getEmail() + " 접근");
         return ResponseEntity.ok().build();
     }
 
